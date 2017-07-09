8ca6ad022b3c920d0c362f2b43883aadf560089d
egesadic <ege.sadic@gmail.com>
2017-07-09 20:54:03 +0300

    Union Dev. Build6
    Bugfixler:
    -Dosya adında boşluk bırakma düzeltildi
    -Boş klasörde dosya döndürme bug'ı düzeltildi
    
    Yenilikler:
    -Rastgele slayt yaratımı eklendi
    -Klasör doluluk kontrolü eklendi

--- createSlide.py
+++ createSlide.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
 from Tkinter import *
 import tkMessageBox
 import os
+from random import shuffle
 from os import listdir, path
 from os.path import isfile, join
 from tkFileDialog import askdirectory
@@ -15,120 +15,124 @@ root = Tk()
 root.wm_title("Create New Image Slideshow")
 root.resizable(0,0)
 
-var = StringVar()
-tmp = StringVar()
-var.set('Select Path...')
-
-def disableButtons():
-    for w in root.winfo_children():
-        w.configure(state="disabled")
-def enableButtons():
-    for wt in root.winfo_children():
-        wt.configure(state="normal")
+def emptymedia():
+    sys.exit("No suitable media found in DeCore.")
 
 def newSlideshow():
     try:
         name = entryName.get()
+        name = name.strip()
         cwd = os.getcwd()
+        filepath = os.path.join(cwd + "/slides", name)
         mediapath = cwd + "/media"
         imgCount = 0
         vidCount = 0
         temp = ""
         init = False
         filelist = [f for f in listdir(mediapath) if isfile(join(mediapath, f))]
-        delay = int(str(entryDelay.get()))
-        lol = ''.join(filelist)
-        if lol is "":
-            print("nothing here man")
+        if not filelist:
+            emptymedia()
         else:
+            lol = ''.join(filelist)
             print ("Found " + str(len(filelist)) + " items: " + lol + " ")
-        fullscript = "#!/bin/bash\ncd " + mediapath + "\n"
-        imgScript = "feh --quiet --preload --reload 60 -Y --slideshow-delay " + str(delay) + ".0 --full-screen --cycle-once "	
-        vidScript = "omxplayer " + mediapath + "/"
-        if name is "":
-            tkMessageBox.showinfo("Warning", "Please name your slide.")
-            print("Slide was unnamed, name your slide.")
-        elif path is "":
-            tkMessageBox.showinfo("Warning", "Please select a path to your images.")
-        elif delay is "0":
-            tkMessageBox.showinfo("Warning", "Please indicate a valid delay between slides.")
-            print("Invalid or unspecified delay interval, assuming 15 second interval")
-        else:
-            print(cwd)
-            filepath = os.path.join(cwd + "/slides", name)
-            slide = open(filepath + '.dpa','w')
-            for file in filelist:
-			    #check init flag
-                print("Now processing: " + file)
-                if init is False:
-                    print ("init was false, this means this is the first media, flagging...")
-					#generate image list and vid name after init
-                    init = True
-                    imgList = []
-                    vidName = ""
-                    if file.endswith(('.jpg', '.jpeg', '.png','.gif')):
-                        print("first media is an image, combo started...\n")
-                        imgList.append(str(file + " "))
-                        imgCount += 1
-                        #print("img's appended, combo started with " +
-                        #''.join(imgList+ " "))
-                    elif file.endswith(('.mp4','.h264')):
-                        print("first media is a video, writing to bash file...\n")
-                        vidName = ''.join([fullscript,vidScript, file, '\n'])
-                        fullscript = vidName
-                        vidCount += 1
-                    else:
-                        print("nothing's here...")
-                        tkMessageBox.showinfo("Warning", "No suitable media found.")
-                else:
-                    print("getting rest of the media...")
-                    print("current status: ImageCount=" + str(imgCount) + " VidCount=" + str(vidCount))
-					#stuff to do after init
-					#if both counters are equal, break the loop.
-                    if vidCount == imgCount:
-                        print("nothing's here...")
-                        tkMessageBox.showinfo("Warning", "No suitable media found.")
-					#image list generator, break the combo if the next media in line isnt an
-                    elif imgCount > vidCount:
-                        print("image combo ongoing, populating image array...\n")
+            if varRand.get():
+                print("Random flag was on, randomizing file list...")
+                shuffle(filelist)
+                lol = ''.join(filelist)
+                print("Randomized list: "+lol+"\n")
+            delay = int(entryDelay.get())      
+            fullscript = "#!/bin/bash\ncd " + mediapath + "\n"
+            imgScript = "feh --quiet --preload --reload 60 -Y --slideshow-delay " + str(delay) + ".0 --full-screen --cycle-once "	
+            vidScript = "omxplayer " + mediapath + "/"
+            if name is "":
+                tkMessageBox.showinfo("Warning", "Please name your slide.")
+                print("Slide was unnamed, name your slide.")
+            elif delay is "0":
+                tkMessageBox.showinfo("Warning", "Please indicate a valid delay between slides.")
+                print("Invalid or unspecified delay interval, assuming 15 second interval")
+            else:
+                slide = open(filepath + '.dpa','w')
+                for file in filelist:
+			        #check init flag
+                    print("Now processing: " + file)
+                    if init is False:
+                        print ("init was false, this means this is the first media, changing flag...")
+				        #generate image list and vid name after init
+                        init = True
+                        imgList = []
+                        vidName = ""
                         if file.endswith(('.jpg', '.jpeg', '.png','.gif')):
+                            print("first media is an image, combo started...\n")
                             imgList.append(file + " ")
                             imgCount += 1
-                            print("Current combo: " + ''.join(imgList))
-                        elif file.endswith((".mp4",".h264")):
-                           print("combo broken!")
-                           imgCount = 0
-                           combinedImg = "".join(imgList)                          
-                           fullscript = ''.join([fullscript, imgScript, combinedImg, '\n', vidScript, file, '\n'])
-                           vidCount += 1                          
-                        else:						
-                            tkMessageBox.showinfo("Warning", "No suitable media found.")					
+                            print("img's appended to list, continuing process...\n")
+                        elif file.endswith(('.mp4','.h264')):
+                            print("first media is a video, writing to bash file...\n")
+                            vidName = ''.join([fullscript,vidScript, file, '\n'])
+                            fullscript = vidName
+                            vidCount += 1
+                        else:
+                            emptymedia()
                     else:
-                        if file.endswith(('.mp4','.h264')):
-                            temp = ''.join([fullscript,vidScript, file, '\n'])
-                            fullscript = temp
-                        elif file.endswith(('.jpg', '.jpeg', '.png','.gif')):
-                            vidCount = 0
-                            imgList = []
-                            imgList.append(file + " ")
-                            imgCount += 1
-            if len(imgList) > 0:
-                print(str(len(imgList))+" images left in array, writing them to file...")
-                combinedImg = ''.join(imgList)
-                fullscript = ''.join([fullscript, imgScript, combinedImg, '\n'])
-            slide.write(fullscript + "exit 0")	
-            slide.close()
-            call("chmod +x " + filepath + ".dpa", shell= True)
-            tkMessageBox.showinfo("Success", "Slideshow '" + name + "' has been successfully created.")
-            root.destroy()
+                        print("getting rest of the media...")
+                        print("current status: ImageCount=" + str(imgCount) + " VidCount=" + str(vidCount)+"\n")
+				        #stuff to do after init
+				        #if both counters are equal, break the loop.
+                        if vidCount == imgCount:
+                            emptymedia()
+				        #image list generator, break the combo if the next media in line isnt an
+                        elif imgCount > vidCount:                            
+                            if file.endswith(('.jpg', '.jpeg', '.png','.gif')):
+                                print("image combo ongoing, populating image array...")
+                                imgList.append(file + " ")
+                                imgCount += 1
+                                print("Current combo: " + ''.join(imgList))
+                            elif file.endswith((".mp4",".h264")):
+                                print("combo broken!")
+                                imgCount = 0
+                                combinedImg = "".join(imgList)                          
+                                fullscript = ''.join([fullscript, imgScript, combinedImg, '\n', vidScript, file, '\n'])
+                                vidCount += 1                          
+                            else:						
+                               emptymedia()					
+                        else:
+                            if file.endswith(('.mp4','.h264')):
+                                temp = ''.join([fullscript,vidScript, file, '\n'])
+                                fullscript = temp
+                                vidCount += 1
+                            elif file.endswith(('.jpg', '.jpeg', '.png','.gif')):
+                                print("img combo started...")
+                                vidCount = 0
+                                imgList = []
+                                imgList.append(file + " ")
+                                imgCount += 1
+                if len(imgList) > 0:
+                    print("\n"+str(len(imgList))+" images left in array after end of operation, writing them to file...")
+                    combinedImg = ''.join(imgList)
+                    fullscript = ''.join([fullscript, imgScript, combinedImg, '\n'])
+                    print("Done writing remainder files...\n")
+                slide.write(fullscript + "exit 0")	
+                slide.close()
+                call("chmod +x " + filepath + ".dpa", shell= True)
+                print("Slide created under dir '"+filepath+".dpa'")
+                tkMessageBox.showinfo("Success", "Slideshow '" + name + "' has been successfully created.")
+                root.destroy()
+                return 0
 
     except Exception as e:
         tkMessageBox.showwarning("Error", e)
         print("There was a problem, aborted slide creation.")
-        os.remove(filepath+name+'.dpa')
+        if os.path.exists(filepath+'.dpa'):
+            print("Removing slide file...")
+            slide.close()
+            os.remove(filepath+'.dpa')
+            print("Removed!")
         entryDelay.delete(0, END)
         entryName.delete(0, END)
         entryDelay.insert(0, 15)
+    except SystemExit as ex:
+        print("No media here, stopping...")
+        tkMessageBox.showwarning("Error", ex)
 
 labelName = Label(root, text = "Slideshow Name: ")
 labelName.grid(row=0, column=0, columnspan=1, sticky="w", padx=10, pady=10)
@@ -141,10 +145,10 @@ labelDelay.grid(row=2, column=0, columnspan=1,sticky="w",padx=10,pady=10)
 entryDelay = Entry(root, width=10)
 entryDelay.grid(row=2, column=1, columnspan=1,sticky="w",padx=10, pady=10)
 
-#varRand = IntVar()
-#checkRand = Checkbutton(root, text='Randomize Images', onvalue=1, offvalue=0,
-#variable = varRand)
-#checkRand.grid(row=3, column=1, columnspan=1, sticky="w", padx=10, pady=10)
+varRand = IntVar()
+checkRand = Checkbutton(root, text='Randomize Media', onvalue=1, offvalue=0,
+variable = varRand)
+checkRand.grid(row=3, column=1, columnspan=1, sticky="w", padx=10, pady=10)
 
 buttonCreate = Button(root, text = "OK", width = 10, command = newSlideshow)
 buttonCreate.grid(row=4, column=1, columnspan=1, sticky="e", padx=10, pady=10)
